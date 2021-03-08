ff7311d9d316c0042d5656fd98589c3178d7b8e9
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-03-08 12:01:45 -0500

    Adding Request Logic

--- ts-redux/src/state/action-creators/index.ts
+++ ts-redux/src/state/action-creators/index.ts
@@ -10,6 +10,20 @@ const searchRepositories = (term: string) => {
 
         try {
 
+            const { data } = await axios.get('https://registry.npmjs.org/-/v1/search', {
+                params: {
+                    text: term
+                }
+            });
+
+            const names = data.object.map((result: any) => {
+                return result.package.name;
+            });
+
+            dispatch({
+                type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+                payload: names
+            });
         } catch (err) {
             dispatch({
                 type: ActionType.SEARCH_REPOSITORIES_ERROR,
