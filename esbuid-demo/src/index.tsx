53440f09e12e01ec40d8af58db4ecf5e0397427c
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-03-09 11:39:53 -0500

    Using Unpkg

--- esbuid-demo/src/index.tsx
+++ esbuid-demo/src/index.tsx
@@ -1,6 +1,7 @@
 import * as esbuild from 'esbuild-wasm';
 import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 const App = () => {
     const ref = useRef<any>();
@@ -24,13 +25,15 @@ const App = () => {
             return;
         }
 
-        /* This operation is asynchronous in nature*/
-        const result = await ref.current.transform(input, {
-            loader: 'jsx', /* Tell what kind of code we're providing to it*/
-            target: 'es2015'/* This is use for the transpiling process*/
+        const result = await ref.current.build({
+            entryPoints: ['index.js'],
+            bundle: true,
+            write: false,
+            plugins: [unpkgPathPlugin()]
         });
 
-        setCode(result.code);
+        //console.log(result);
+        setCode(result.outputFiles[0].text);
     };
 
     return <div>
