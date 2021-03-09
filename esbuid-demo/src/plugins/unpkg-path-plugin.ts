95433bff11a5ab409c938bf128d8e1ce750e1e37
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-03-09 14:50:04 -0500

    Resolving Nested Path

--- esbuid-demo/src/plugins/unpkg-path-plugin.ts
+++ esbuid-demo/src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,6 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
+import { request } from 'node:http';
 export const unpkgPathPlugin = () => {
     return {
         name: 'unpkg-path-plugin',
@@ -13,7 +14,7 @@ export const unpkgPathPlugin = () => {
                 if (args.path.includes('./') || args.path.includes('../')) {
                     return {
                         namespace: 'a',
-                        path: new URL(args.path, args.importer + '/').href
+                        path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href
                     };
                 }
 
@@ -35,15 +36,16 @@ export const unpkgPathPlugin = () => {
                     return {
                         loader: 'jsx',
                         contents: `
-                        const message = require('medium-test-pkg');
+                        const message = require('nested-test-pkg');
                         console.log(message);
                         `,
                     };
                 }
-                const { data } = await axios.get(args.path);
+                const { data, request } = await axios.get(args.path);
                 return {
                     loader: 'jsx',
-                    contents: data
+                    contents: data,
+                    resolveDir: new URL('./', request.responseURL).pathname
                 };
             });
         },
