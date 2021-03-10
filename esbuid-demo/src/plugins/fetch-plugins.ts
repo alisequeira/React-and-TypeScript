48261d9db1cd8b1be7e2a5e55a3d63ce75cc0d43
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-03-10 12:32:11 -0500

    Escaping CSS Snippets

--- esbuid-demo/src/plugins/fetch-plugins.ts
+++ esbuid-demo/src/plugins/fetch-plugins.ts
@@ -21,17 +21,32 @@ export const fetchPlugin = (inputCode: string) => {
 
                 //Check if we see if we have already fetched this file
                 //and if it is in the cache
+                /* 
                 const cacheResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
 
                 //if it is return it immediately 
                 if (cacheResult) {
                     return cacheResult;
                 }
+                */
                 const { data, request } = await axios.get(args.path);
+
+                const fileType = args.path.match(/.css$/) ? 'css' : 'jsx';
+
+                const escaped = data
+                    .replace(/\n/g, '')
+                    .replace(/"/g, '\\"')
+                    .replace(/'/g, "\\'")
+                const contents = fileType === 'css' ?
+                    `
+                        const style = document.createElement('style');
+                        style.innerText = '${escaped}';
+                        document.head.appendChild(style);
+                    ` : data;
 
                 const result: esbuild.OnLoadResult = {
                     loader: 'jsx',
-                    contents: data,
+                    contents,
                     resolveDir: new URL('./', request.responseURL).pathname
                 };
                 //store response in cache
