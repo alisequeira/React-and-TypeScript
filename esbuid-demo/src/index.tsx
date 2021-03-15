d9da164f68c5123103872ee59eb11c0d14e49212
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-03-15 15:40:56 -0400

    Displaying an Editor

--- esbuid-demo/src/index.tsx
+++ esbuid-demo/src/index.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugins';
+import CodeEditor from './components/code-editor';
 
 const App = () => {
     const ref = useRef<any>();
@@ -66,6 +67,7 @@ const App = () => {
     `
 
     return <div>
+        <CodeEditor />
         <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
         <div>
             <button onClick={onClick}>Submit</button>
