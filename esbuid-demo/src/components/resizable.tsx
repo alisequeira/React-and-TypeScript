b501462cc34ae6da8d661d258ac048f09e0b3d0f
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-03-17 16:07:47 -0400

    On Resize Events

--- esbuid-demo/src/components/resizable.tsx
+++ esbuid-demo/src/components/resizable.tsx
@@ -1,6 +1,6 @@
 import './resizable.css';
 import { ResizableBox, ResizableBoxProps } from 'react-resizable';
-
+import { useEffect } from 'react'
 interface ResizableProps {
     direction: 'horizontal' | 'vertical';
 }
@@ -10,13 +10,25 @@ interface ResizableProps {
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
     let resizableProps: ResizableBoxProps;
 
+    useEffect(() => {
+        const listener = () => {
+            console.log(window.innerWidth, window.innerHeight);
+        };
+        window.addEventListener('resize', listener);
+
+        return () => {
+            window.removeEventListener('resize', listener);
+        }
+    }, [])
+
     if (direction === 'horizontal') {
         resizableProps = {
+            className: 'resize-horizontal',
             height: Infinity,
             width: window.innerWidth * 0.75,
             resizeHandles: ['e'],
-            maxConstraints: [window.innerWidth * 0.2, Infinity],
-            minConstraints: [window.innerWidth * 0.75, Infinity]
+            maxConstraints: [window.innerWidth * 0.75, Infinity],
+            minConstraints: [window.innerWidth * 0.2, Infinity]
         };
     } else {
         resizableProps = {
