0bfb5f92050dab4c45e77e8e71000f3ed6c10862
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-03-17 15:30:30 -0400

    Applying Horizontal Resize Props

--- esbuid-demo/src/components/resizable.tsx
+++ esbuid-demo/src/components/resizable.tsx
@@ -12,21 +12,23 @@ const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
 
     if (direction === 'horizontal') {
         resizableProps = {
-
+            height: Infinity,
+            width: window.innerWidth * 0.75,
+            resizeHandles: ['e'],
+            maxConstraints: [window.innerWidth * 0.2, Infinity],
+            minConstraints: [window.innerWidth * 0.75, Infinity]
         };
     } else {
         resizableProps = {
-
+            height: 300,
+            width: Infinity,
+            resizeHandles: ['s'],
+            maxConstraints: [Infinity, window.innerHeight * 0.9],
+            minConstraints: [Infinity, 24]
         };
     }
     return (
-        <ResizableBox
-            height={300}
-            width={Infinity}
-            resizeHandles={['s']}
-            maxConstraints={[Infinity, window.innerHeight * 0.9]}
-            minConstraints={[Infinity, 24]}
-        >
+        <ResizableBox {...resizableProps}>
             {children}
         </ResizableBox>
     );
