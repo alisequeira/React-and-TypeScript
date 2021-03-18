31642e0ea035c0974b52a46320ed66ec5c8436bd
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-03-18 11:46:13 -0400

    Extracting Reusable Error Handling Logic

--- esbuid-demo/src/components/preview.tsx
+++ esbuid-demo/src/components/preview.tsx
@@ -11,13 +11,17 @@ const html = `
     <body>
         <div id="root"></div>
         <script>
+            const handleError =(err) => {
+                const root = document.querySelector('#root');
+                root.innerHTML = '<div style="color: red;"><h4>RunTime Error</h4>'+ err +'</div>'
+                throw err;
+            };
+
             window.addEventListener('message', (event)=> {
                 try{
                     eval(event.data)
                 }catch(err){
-                    const root = document.querySelector('#root');
-                    root.innerHTML = '<div style="color: red;"><h4>RunTime Error</h4>'+ err +'</div>'
-                    throw err;
+                    handleError(err);
                 }
             }, false)
         </script>
