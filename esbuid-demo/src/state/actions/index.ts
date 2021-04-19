80c875d38b31d72184c991a32332509653a9bce0
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-04-19 12:30:04 -0400

    Action Creator Return Type

--- esbuid-demo/src/state/actions/index.ts
+++ esbuid-demo/src/state/actions/index.ts
@@ -1,6 +1,6 @@
 import { ActionType } from "../action-types";
 import { CellType } from "../cell";
-interface MoveCellAction {
+export interface MoveCellAction {
   type: ActionType.MOVE_CELL;
   payload: {
     id: string;
@@ -8,12 +8,12 @@ interface MoveCellAction {
   };
 }
 
-interface DeleteCellAction {
+export interface DeleteCellAction {
   type: ActionType.DELETE_CELL;
   payload: string;
 }
 
-interface InsertCellBeforeAction {
+export interface InsertCellBeforeAction {
   type: ActionType.INSERT_CELL_BEFORE;
   payload: {
     id: string;
@@ -21,7 +21,7 @@ interface InsertCellBeforeAction {
   };
 }
 
-interface UpdateCellAction {
+export interface UpdateCellAction {
   type: ActionType.UPDATE_CELL;
   payload: {
     id: string;
