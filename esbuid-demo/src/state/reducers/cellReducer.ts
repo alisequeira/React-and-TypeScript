b4274fc41173f4a7d8c473fdf81e40e25bd37de4
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-04-19 14:29:49 -0400

    Insert New Cell

--- esbuid-demo/src/state/reducers/cellReducer.ts
+++ esbuid-demo/src/state/reducers/cellReducer.ts
@@ -32,12 +32,43 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
       return;
 
     case ActionType.MOVE_CELL:
-      return state;
+      const { direction } = action.payload;
+      const index = state.order.findIndex((id) => id === action.payload.id);
+      const targetIndex = direction === "up" ? index - 1 : index + 1;
+
+      if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+        return;
+      }
+
+      state.order[index] = state.order[targetIndex];
+      state.order[targetIndex] = action.payload.id;
+      return;
+
     case ActionType.INSERT_CELL_BEFORE:
+      const cell: Cell = {
+        content: "",
+        type: action.payload.type,
+        id: randomId(),
+      };
+
+      state.data[cell.id] = cell;
+
+      const foundIndex = state.order.findIndex(
+        (id) => id === action.payload.id
+      );
+      if (foundIndex < 0) {
+        state.order.push(cell.id);
+      } else {
+        state.order.splice(foundIndex, 0, cell.id);
+      }
+
       return state;
     default:
       return state;
   }
 });
 
+const randomId = () => {
+  return Math.random().toString(36).substr(2, 5);
+};
 export default reducer;
