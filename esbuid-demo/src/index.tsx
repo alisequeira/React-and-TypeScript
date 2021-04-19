5ee07b529cb31ee8913743040be9c23f9a4fb257
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-04-19 12:11:24 -0400

    React to Redux Connection

--- esbuid-demo/src/index.tsx
+++ esbuid-demo/src/index.tsx
@@ -1,13 +1,17 @@
-import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import ReactDOM from 'react-dom';
-//import CodeCell from './components/code-sell';
-import TextEditor from './components/text-editor'
+import "bulmaswatch/superhero/bulmaswatch.min.css";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { store } from "./state";
+import TextEditor from "./components/text-editor";
 
 const App = () => {
-
-    return <div>
+  return (
+    <Provider store={store}>
+      <div>
         <TextEditor />
-    </div>
+      </div>
+    </Provider>
+  );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
