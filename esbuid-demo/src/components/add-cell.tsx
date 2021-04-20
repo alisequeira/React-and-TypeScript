9a253722a069580b5d21ce4cc086acce296b4f5b
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-04-20 13:38:08 -0400

    Adding New Cells

---
+++ esbuid-demo/src/components/add-cell.tsx
@@ -0,0 +1,18 @@
+import "add-cell.css";
+import { useAction } from "../hooks/use-actions";
+
+interface AddCellProps {
+  nextCellId: string;
+}
+
+const AddCell: React.FC<AddCellProps> = ({ nextCellId }) => {
+  const { insertCellBefore } = useAction();
+  return (
+    <div>
+      <button onClick={() => insertCellBefore(nextCellId, "code")}>Code</button>
+      <button onClick={() => insertCellBefore(nextCellId, "text")}>Text</button>
+    </div>
+  );
+};
+
+export default AddCell;
