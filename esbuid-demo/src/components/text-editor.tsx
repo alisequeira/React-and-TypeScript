8aede018e797a3bab1cf8026b2ed3612f214b8dc
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-04-20 12:31:11 -0400

    Extracting State from Text Cell

--- esbuid-demo/src/components/text-editor.tsx
+++ esbuid-demo/src/components/text-editor.tsx
@@ -1,39 +1,51 @@
-import './text-editor.css';
-import MDEditor from '@uiw/react-md-editor';
-import { useState, useEffect, useRef } from 'react'
+import "./text-editor.css";
+import MDEditor from "@uiw/react-md-editor";
+import { useState, useEffect, useRef } from "react";
+import { Cell } from "../state";
+import { useAction } from "../hooks/use-actions";
+interface TextEditorProps {
+  cell: Cell;
+}
 
-const TextEditor: React.FC = () => {
-    const [editing, setEditing] = useState(false);
-    const ref = useRef<HTMLDivElement | null>(null);
-    const [value, setValue] = useState('# Hi there!')
-    useEffect(() => {
-        const listener = (event: MouseEvent) => {
-            if (ref.current && event.target && ref.current.contains(event.target as Node)) {
-                return;
-            }
-            setEditing(false);
-        };
-        document.addEventListener('click', listener, { capture: true });
+const TextEditor: React.FC<TextEditorProps> = ({ cell }) => {
+  const [editing, setEditing] = useState(false);
+  const ref = useRef<HTMLDivElement | null>(null);
+  const { updateCell } = useAction();
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        return;
+      }
+      setEditing(false);
+    };
+    document.addEventListener("click", listener, { capture: true });
 
-        return () => {
-            document.removeEventListener('click', listener, { capture: true });
-        };
-    }, [])
+    return () => {
+      document.removeEventListener("click", listener, { capture: true });
+    };
+  }, []);
 
-    if (editing) {
-        return (
-            <div className="text-editor" ref={ref}>
-                <MDEditor value={value} onChange={(v) => setValue(v || '')} />
-            </div>
-        )
-    }
+  if (editing) {
     return (
-        <div className="text-editor card" onClick={() => setEditing(true)}>
-            <div className="card-content">
-                <MDEditor.Markdown source={value} />
-            </div>
-        </div>
-    )
+      <div className="text-editor" ref={ref}>
+        <MDEditor
+          value={cell.content}
+          onChange={(v) => updateCell(cell.id, v || "")}
+        />
+      </div>
+    );
+  }
+  return (
+    <div className="text-editor card" onClick={() => setEditing(true)}>
+      <div className="card-content">
+        <MDEditor.Markdown source={cell.content || "Click to Edit"} />
+      </div>
+    </div>
+  );
 };
 
 export default TextEditor;
