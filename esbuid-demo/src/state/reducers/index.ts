bb59eab2e0c9232abcd7743720f24305d3460b1e
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-04-21 12:49:57 -0400

    Finishing Up reducers

--- esbuid-demo/src/state/reducers/index.ts
+++ esbuid-demo/src/state/reducers/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import cellReducer from "./cellReducer";
+import bundlesReducer from "./bundlesReducer";
 
 const reducer = combineReducers({
   cells: cellReducer,
+  bundles: bundlesReducer,
 });
 
 export default reducer;
